7a83a28ad4fbe2624b72fea010f93bb4ebb4f843
AbdulkadirMusa <abdulkadirmusa467@gmail.com>
2022-03-14 08:49:30 +0000

    My commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+	int n = 0;
+
+	while (n < 52)
+	{
+		if (n < 26)
+		{
+			putchar(a);
+			a++;
+		} else
+		{
+			putchar(a);
+			a++;
+		} else
+		{
+			putchar(A);
+			A++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
