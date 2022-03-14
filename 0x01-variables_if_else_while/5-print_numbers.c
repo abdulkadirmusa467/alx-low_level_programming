3fb2e091ae0de25522a20b6a4de2a0b178ff50dd
AbdulkadirMusa <abdulkadirmusa467@gmail.com>
2022-03-14 13:52:27 +0000

    My commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Program entry point
- *
- * Return: 0 on success. Error code otherwise
- */
-int main(voiid)
+* main - Program entry point
+*
+* Return: 0 on success. Error code otherwise
+*/
+int main(void)
 {
 int n;
 for (n = 0; n < 10; n++)
