751b57bfa60e6252c8f2a653a158101f562e8e58
AbdulkadirMusa <abdulkadirmusa467@gmail.com>
2022-03-14 12:50:08 +0000

    My commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - program entry point
+ *
+ *  Return: 0 on success. Error code otherwise
+ */
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
-	
-
+for (int i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
