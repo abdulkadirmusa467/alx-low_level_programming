847950f3091a65711d5e9f7b8a5246f863b8f483
AbdulkadirMusa <abdulkadirmusa467@gmail.com>
2022-07-01 08:37:01 +0000

    Tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht;
-if (size == 0)
-return (NULL);
-ht = malloc(sizeof(hash_table_t));
-if (!ht)
-return (NULL);
-ht->size = size;
-ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-if (ht->array == NULL)
-return (NULL);
-return (ht);
+	hash_table_t *ht;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+		return (NULL);
+
+	return (ht);
 }
